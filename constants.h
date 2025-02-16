8c0972092c6252ebe408fb045d376014f2f1fc99
Shruti Pawar <shruti.all1111@gmail.com>
2025-02-16 20:53:37 +0530

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,57 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+//These constants were derived by printing the values after scaler function in the python code. 
+//Note these will vary with your data
+//For standardization 
+#define ax_mean_scaler_std -0.0409250268757396 
+#define ax_mean_scalermean 0.0782356711388434 
+#define ax_std_scaler_std 0.103167799507648 
+#define ax_std_scaler_mean 0.0918289713436291 
+#define ax_max_scalerstd 0.195434553846153 
+#define ax_maxscalermean 0.191550588605888 
+#define ax_min_scaler_std -0.393851446153846 
+#define ax_min_scaler_mean 0.430132392517396 
+#define ay_mean_scaler_std -0.0755258111597633 
+#define ay_mean_scaler_mean 0.19606456285295 
+#define ay_std_scaler_std 0.246656858248513 
+#define ay_std_scaler_mean 0.203592035118468 
+#define ay_max_scaler_std 0.599723892307692 
+#define ay_max_scalermean 0.699618346204252 
+#define ay_min_scaler_std -0.79266823076923 
+#define ay_min_scaler_mean 0.709425008814858 
+#define az_mean_scaler_std -0.0593152707455621 
+#define az_mean_scaler_mean 0.902629497346466 
+#define az_std_scaler_std 0.349274987515841 
+#define az_std_scaler_mean 0.362482732905027 
+#define az_max_scaler_std 1.0449801076923 
+#define az_max_scaler_mean 1.67376920040809 
+#define az_min_scaler_std -0.774470353846153 
+#define az_min_scaler_mean 1.16685123260142 
+#define gx_mean_scaler_std -1.05564453133727 
+#define gx_mean_scaler_mean 5.01890072090085 
+#define gx_std_scaler_std 61.2952484770434 
+#define gx_std_scaler_mean 70.3266216745634 
+#define gx_max_scaler_std 205.250888923076 
+#define gx_max_scaler_mean 227.645297730722 
+#define gx_min_scaler_std -211.818275461538 
+#define gx_min_scaler_mean 248.127255308494 
+#define gy_mean_scaler_std 0.72142183668639 
+#define gy_mean_scaler_mean 3.6681441476026 
+#define gy_std_scaler_std 43.3410658667379 
+#define gy_std_scaler_mean 45.1476612988826 
+#define gy_max_scaler_std 126.469533584615 
+#define gy_max_scaler_mean 125.574533191281 
+#define gy_min_scaler_std -134.347761138461 
+#define gy_min_scaler_mean 151.004826205172 
+#define gz_mean_scaler_std -1.41367675052071 
+#define gz_mean_scaler_mean 6.47020310100262 
+#define gz_std_scaler_std 30.9360148271667 
+#define gz_std_scaler_mean 28.5187534049953 
+#define gz_max_scaler_std 99.8638409538461 
+#define gz_max_scaler_mean 102.523293471137 
+#define gz_min_scaler_std -99.6394187384615 
+#define gz_min_scaler_mean 94.276137495953 
+
+
+#endif
---
+++ model.h
@@ -0,0 +1,7 @@
+#ifndef MODEL_H_
+#define MODEL_H_
+
+extern const unsigned char model_data[];
+extern const int model_len;
+
+#endif //tensorflow lite model 
